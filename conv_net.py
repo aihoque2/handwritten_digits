b4bec057ca8dbdb2cb780aa39fbb35c7de51797c
aihoque2 <ahoque245@gmail.com>
2020-07-09 04:33:05 -0500

    structure developed

--- conv_net.py
+++ conv_net.py
@@ -1,4 +1,4 @@
-##here is the implementation of the convolutional neural network!
+##here is the implem1entation of the convolutional neural network!
 import numpy as np
 import torch
 import torch.nn as nn
@@ -8,27 +8,47 @@ import torch.optim as optim
 
 
 class convNet(torch.nn.Module):
-    def __init__(self, lrate, loss_fn, in_size, out_size):
+    def __init__(self, lrate, in_size, out_size):
     
     super(convNet, self).__init__()
     
     self.loss_fn =loss_fn
+    self.conv1 = nn.Conv2d(1, 10, 5)
+    self.hidden1 = nn.linear(320, 100) #put the pooled features through a hidden layer
+    self.output = nn.linear(100, 10) #this layer classifies for us
 
+    self.reLu = nn.ReLU()
+    self.pool = nn.MaxPool2d(2,2)
 
 
     def get_parameters(self):
         return self.parameters()
 
     def forward(self, x)
-    #implement the forward algo here!
-
+    #implement forward propogation
+    
+    x = x.view(-1, 1, 28, 28)
+    x = self.pool(self.relu(self.conv1(x)))
+    x = x.view(-1, 10*12*12)
+    
+    x = self.relu(self.fc1(x))
+    x = self.output(x)
+    
+    return x
 
     def step(self, x, y)
     #perform a gradient descent step
 
+    outputs = self.forward(x)
 
+    #get the loss and backpropagate it
+    loss = self.criterion(outputs, y)
+    loss.backward()
 
+    self.optimizer.step()
+    L = loss.item()
 
+    return L
 
 
 
