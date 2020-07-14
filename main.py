ead9c0d9d8c56d13cb47a73f4b4701b3577e3258
aihoque2 <ahoque245@gmail.com>
2020-07-14 15:12:52 -0500

    moving over to my better machine

---
+++ main.py
@@ -0,0 +1,7 @@
+from mlxtend.data import loadlocal_mnist
+
+X, y = loadlocal_mnist(images_path = '/home/ahoque245/projects/AI/handwritten_digits/data/train/train-images-idx3-ubyte',
+ labels_path='/home/ahoque245/projects/AI/handwritten_digits/data/train/train-labels-idx1-ubyte')
+
+print('Dimensions: %s x %s' % (X.shape[0], X.shape[1]))
+print('\n1st row', X[0])
